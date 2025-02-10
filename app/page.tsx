c4dc246a7b64af1bad70e4fc7784e4f055e01d3f
YeonhaPark <yonaprisca@gmail.com>
2025-02-10 21:07:21 +0900

    feat: create sphere component

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,25 @@
 import Image from "next/image";
-
+import GradientSphere from "@/components/sphere";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-work-sans)]">
+      <main className="w-96 flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div>
+          <div className="flex space-x-[-40px]">
+            <GradientSphere size={'sm'}/>
+            <GradientSphere size={'lg'}/>
+            <GradientSphere size={'md'}/>
+          </div>
+          <span>Hello, I'm Yeonha, </span>
+
+          <span>
+            A Web3 frontend engineer based in Seoul.
+          </span>
+          <p>
+            I make beautiful websites and crypto applications. I am enthusiastic learner and communicator. I'm currently
+            looking for a new development role. Hire me?
+          </p>
+        </div>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
