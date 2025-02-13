5b80d30a407e3c447809d0161ed689d0e13c8f1e
YeonhaPark <yonaprisca@gmail.com>
2025-02-13 15:59:01 +0900

    feat: create template component for section

---
+++ components/section-template.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from "react";
+
+export default function SectionTemplate({
+    title, children }: Readonly<{
+    title: string,
+    children: React.ReactNode;
+}>){
+    return <section className={"pb-14 border-b border-gray-300 m-auto"}>
+        <h1 className={"text-3xl font-semibold text-foreground mb-8"}>{title}</h1>
+        {children}
+    </section>
+}
