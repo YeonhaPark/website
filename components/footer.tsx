1ac6dffa2d8a1333129aa60f921d467b56185870
YeonhaPark <yonaprisca@gmail.com>
2025-03-28 02:42:21 +0900

    fix: footer github link

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return <footer className="mt-20 py-5 row-start-3 flex gap-6 flex-wrap items-center justify-center text-xs">
       <div className={"flex gap-4"}>
-          <a href="github.com/YeonhaPark" target={"_blank"} rel={"noreferrer"}>
+          <a href="https://github.com/YeonhaPark" target={"_blank"} rel={"noreferrer"}>
           <span className={"icon-github"}/></a>
           <a href="https://www.linkedin.com/in/yonapark/" target={"_blank"} rel={"noreferrer"}>
           <span className={"icon-linkedin"}/></a>
