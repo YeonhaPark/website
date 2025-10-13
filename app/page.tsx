4e3985f492ea107294046838aec17a1d0892f535
YeonhaPark <yonaprisca@gmail.com>
2025-10-13 19:20:08 +0900

    feat: add footer

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,14 @@ import SectionTemplate from "@/components/section-template";
 import Card from "@/components/card";
 import HeroText from "@/components/hero-text";
 import { sideProjects } from "@/constants";
-import React from "react";
 import GradientTemplate from "@/components/gradient-template";
-import Footer from "@/components/footer";
 import ArticleList from "@/components/article-list";
 import { getSortedArticles } from "@/lib/articles";
 import { Hero } from "@/components/hero";
 import { About } from "@/app/section/about";
 import { Projects } from "@/app/section/projects";
 import { Contact } from "./section/contact";
+import { Footer } from "./section/footer";
 export default function Home() {
   const articles = getSortedArticles().slice(0, 3);
 
@@ -20,6 +19,7 @@ export default function Home() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </>
   );
 }
